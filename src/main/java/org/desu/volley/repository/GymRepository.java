0c90ce94b400c1c95dcd43c21d64f5778d5b9a35
John Doe <johndoe@example.com>
2016-12-29 12:16:49 +0500

    удаление поправлено: 1. залы этого города остаются но с пустым городом, надо в админке их править 2. у пользователей в удаденном городе этот город сбрасывается в NULL, им надо будет на главной странице повторно выбрать город
    SQL
    ALTER TABLE public.gym DROP CONSTRAINT fk_gym_city_id;
    
    ALTER TABLE public.gym
      ADD CONSTRAINT fk_gym_city_id FOREIGN KEY (city_id)
          REFERENCES public.city (id) MATCH SIMPLE
          ON UPDATE CASCADE ON DELETE SET NULL;
    
    delete from jhi_user_authority where user_id in (
      select id from jhi_user where city_id not in (select id from city)
    );
    
    delete from jhi_user where city_id not in (select id from city);
    
    ALTER TABLE public.jhi_user
      ADD CONSTRAINT fk_jhi_user_city_id FOREIGN KEY (city_id)
          REFERENCES public.city (id) MATCH SIMPLE
          ON UPDATE CASCADE ON DELETE SET NULL;

--- src/main/java/org/desu/volley/repository/GymRepository.java
+++ src/main/java/org/desu/volley/repository/GymRepository.java
@@ -3,6 +3,7 @@ package org.desu.volley.repository;
 import org.desu.volley.domain.Gym;
 
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,5 +12,8 @@ import java.util.List;
  */
 @SuppressWarnings("unused")
 public interface GymRepository extends JpaRepository<Gym,Long> {
+
+    @Query("select gym from Gym gym where gym.id =:id")
+    Gym findWithEagerCity(@Param("id") Long id);
 
 }
