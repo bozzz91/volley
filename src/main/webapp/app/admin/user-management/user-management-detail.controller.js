2af9c4b632c233d7cf98852ee2258462a4f4bfe9
desu <bozzz91@gmail.com>
2017-03-28 03:15:30 +0300

    Немного доделано отображение профиля юзера, сделано в виде md card

--- src/main/webapp/app/admin/user-management/user-management-detail.controller.js
+++ src/main/webapp/app/admin/user-management/user-management-detail.controller.js
@@ -12,13 +12,25 @@
 
         vm.load = load;
         vm.user = {};
+        vm.userRole = '';
 
         vm.load($stateParams.login);
 
         function load (login) {
             User.get({login: login}, function(result) {
                 vm.user = result;
+                vm.userRole = detectUserRole(vm.user);
             });
+        }
+
+        function detectUserRole(user) {
+            if (user.authorities.indexOf('ROLE_SUPERADMIN') > 0) {
+                return 'Суперадмин';
+            } else if (user.authorities.indexOf('ROLE_ADMIN') > 0) {
+                return 'Админ';
+            } else {
+                return 'Юзер';
+            }
         }
     }
 })();
