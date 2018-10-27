8a5d20871964ba4c00d37fe13fda529aed367766
bozzz91 <bozzz91@gmail.com>
2018-10-27 16:08:35 +0300

    added warning in log about SMS Stub usage instead of real SMS Sender

--- src/main/java/org/desu/volley/service/sms/DevStubSender.java
+++ src/main/java/org/desu/volley/service/sms/DevStubSender.java
@@ -1,12 +1,21 @@
 package org.desu.volley.service.sms;
 
+import lombok.extern.slf4j.Slf4j;
 import org.desu.volley.config.Constants;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
+@Slf4j
 @Service
 @Profile("!" + Constants.SPRING_PROFILE_SMS)
 public class DevStubSender implements SmsSender {
+
+    @PostConstruct
+    public void init() {
+        log.warn("SMS STUB ACTIVATED");
+    }
 
     @Override
     public String[] sendSms(String phones, String message, int translit, String time, String id, int format, String sender, String query) {
